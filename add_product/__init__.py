700901cd534ba635d963bc269ad18885d03c8b69
suman-deb <suman.deb@outlook.com>
2025-05-05 15:55:18 +0300

    Update __init__.py

--- add_product/__init__.py
+++ add_product/__init__.py
@@ -8,8 +8,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Adding product.")
     try:
         data = req.get_json()
+        logging.info(f"Received data: {data}")
         container = get_container()
         print("Connection string is:", COSMOS_CONNECTION_STRING)
+        if "id" not in data or "name" not in data or "price" not in data:
+            raise ValueError("Missing one or more required fields")
 
         data["id"] = str(data.get("id") or str(len(list(container.read_all_items())) + 1))
         container.create_item(body=data)
