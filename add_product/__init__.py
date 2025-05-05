f4d2c269125691624952d399d6391fbe4bb28e55
suman-deb <suman.deb@outlook.com>
2025-05-05 16:04:30 +0300

    Update __init__.py

--- add_product/__init__.py
+++ add_product/__init__.py
@@ -3,14 +3,15 @@ import logging
 import json
 import azure.functions as func
 from shared.cosmos import get_container
-
+import os
+logging.info(f"1. COSMOS_CONNECTION_STRING: {os.getenv('COSMOS_DB_CONNECTION')}")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Adding product.")
     try:
         data = req.get_json()
         logging.info(f"Received data: {data}")
         container = get_container()
-        logging.info(f"Using Cosmos DB connection string: {COSMOS_CONNECTION_STRING}")
+        logging.info(f"2. Using Cosmos DB connection string: {COSMOS_CONNECTION_STRING}")
         if "id" not in data or "name" not in data or "price" not in data:
             raise ValueError("Missing one or more required fields")
 
